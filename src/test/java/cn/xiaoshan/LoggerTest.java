e8c44615cd24dba59ec4fdcb89588e22ecc0eb23
XiaoShan2016 <295971920@qq.com>
2020-09-01 18:19:55 +0800

    update logback-spring config

--- src/test/java/cn/xiaoshan/LoggerTest.java
+++ src/test/java/cn/xiaoshan/LoggerTest.java
@@ -1,5 +1,6 @@
 package cn.xiaoshan;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -11,18 +12,24 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @Author : XiaoShan
  * @Contact_QQ : 295971920
  * @CreateDate : 2020-06-04 22:04
- * @Description :
+ * @Description : 使用Slf4j需导入lombok依赖
  **/
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class LoggerTest {
 
     private final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
 
     @Test
     public void test(){
+        /*Ctrl+Shift+Alt+N 搜索Level 选org.slf4j.event 可以查看级别*/
+        String name = "XiaoShan";
+        String password = "xiaoshan.cn";
+        logger.info("name:"+name+" ,Password:"+password);
+        log.info("name:{}"+ ",password:"+name,password);
         logger.debug("debug...");
         logger.info("info...");
-        logger.error("error...");
+        log.error("error...");
     }
 }

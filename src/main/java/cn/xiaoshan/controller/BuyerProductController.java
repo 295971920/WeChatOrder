332927c69e2c76d3065538ff41fff77b556179bf
XiaoShan2016 <295971920@qq.com>
2020-09-14 00:19:14 +0800

    优化买家商品API

--- src/main/java/cn/xiaoshan/controller/BuyerProductController.java
+++ src/main/java/cn/xiaoshan/controller/BuyerProductController.java
@@ -3,15 +3,20 @@ package cn.xiaoshan.controller;
 import cn.xiaoshan.VO.ProductInfoVO;
 import cn.xiaoshan.VO.ProductVO;
 import cn.xiaoshan.VO.ResultVO;
+import cn.xiaoshan.dataobject.ProductCategory;
+import cn.xiaoshan.dataobject.ProductInfo;
 import cn.xiaoshan.service.CategoryService;
 import cn.xiaoshan.service.ProductService;
+import cn.xiaoshan.utils.ResultVOUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @Author : XIAOSHAN
@@ -28,26 +33,55 @@ public class BuyerProductController {
 
     @Autowired
     private CategoryService categoryService;
-
     @GetMapping("/list")
     public ResultVO list(){
-        ResultVO resultVO = new ResultVO();
-        ProductVO productVO = new ProductVO();
-        ProductInfoVO productInfoVO = new ProductInfoVO();
+        //1. 查询所有的上架商品
+        List<ProductInfo> productInfoList = productService.findUpAll();
 
-        productVO.setCategoryName("热榜");
-        productVO.setCategoryType(1);
-        productInfoVO.setProductId("21348");
-        productInfoVO.setProductName("皮皮虾");
-        productInfoVO.setProductPrice(new BigDecimal(3.2));
-        productInfoVO.setProductIcon("http://xxxx.png");
-        productInfoVO.setProductDescription("非常好吃");
+        //2. 查询类目(一次性查询) 使用传统方法for循环或者使用Java8 新特性Lambda表达式
 
-        productVO.setProductInfoVOList(Arrays.asList(productInfoVO));
-        resultVO.setData(Arrays.asList(productVO));
-        
-        resultVO.setCode(0);
+        //传统方法
+       /* List<Integer> categoryTypeList = new ArrayList<>();
+        for (ProductInfo productInfo : productInfoList) {
+            categoryTypeList.add(productInfo.getCategoryType());
+        }*/
+
+        //精简方法(java8, lambda)
+        List<Integer> categoryTypeList = productInfoList.stream()
+                .map(e -> e.getCategoryType())
+                .collect(Collectors.toList());
+        List<ProductCategory> productCategoryList = categoryService.findByCategoryTypeIn(categoryTypeList);
+
+        //3. 数据拼装
+        List<ProductVO> productVOList = new ArrayList<>();
+        for (ProductCategory productCategory : productCategoryList) {
+            ProductVO productVO = new ProductVO();
+            productVO.setCategoryType(productCategory.getCategoryType());
+            productVO.setCategoryName(productCategory.getCategoryName());
+            List<ProductInfoVO> productInfoVOList = new ArrayList<>();
+            for (ProductInfo productInfo: productInfoList) {
+                //product_info商品表与product_category类目表编号比较
+                if (productInfo.getCategoryType().equals(productCategory.getCategoryType())) {
+                    ProductInfoVO productInfoVO = new ProductInfoVO();
+                    //拷贝数据
+                    BeanUtils.copyProperties(productInfo, productInfoVO);
+                    //将前段需要的商品拷贝并添加到productInfoVOList对象
+                    productInfoVOList.add(productInfoVO);
+                }
+            }
+            productVO.setProductInfoVOList(productInfoVOList);
+            productVOList.add(productVO);
+        }
+
+        /*未封装*/
+        /*ResultVO resultVO = new ResultVO();
         resultVO.setMsg("成功");
-        return resultVO;
+        resultVO.setCode(0);
+        resultVO.setData(productVOList);
+        return resultVO;*/
+
+        /*封装*/
+        return ResultVOUtil.success(productVOList);
     }
+
 }

2f82b4b6defead37b3c4df6f5f656ced4bcf8fb9
XiaoShan2016 <295971920@qq.com>
2020-10-18 01:18:45 +0800

    使用原生实现网页授权

---
+++ src/main/java/cn/xiaoshan/controller/WeiXinController.java
@@ -0,0 +1,29 @@
+package cn.xiaoshan.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Author : XIAOSHAN
+ * @Contact_QQ : 295971920
+ * @CreateDate : 2020/10/18 0:34
+ * @Description : 微信授权
+ **/
+@RestController
+@RequestMapping("/weixin")
+@Slf4j
+public class WeiXinController {
+
+    @GetMapping("/auth")
+    public void auth(@RequestParam("code")String code){
+        log.info("调用了author方法");
+        log.info("code={}",code);
+        //获取code后，请求以下链接获取access_token
+        String url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxbbcc880c6c06faae&secret=SECRET&code=" + code + "&grant_type=authorization_code";
+        //url为json，使用RestTemplate模板
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(url, String.class);
+        log.info("response={}",response);
+    }
+}
